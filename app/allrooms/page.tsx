14bed658c418673b4390b62937f0a4f35e34fb9b
NAVIN0507 <navinofficial2005@gmail.com>
2025-04-30 14:55:28 +0530

    Completed ALL rooms section

--- app/allrooms/page.tsx
+++ app/allrooms/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const page = () => {
   return (
   <>
-  <main className='p-4 flex flex-col gap-10'>
+  <main className='p-4 flex flex-col gap-10 mb-20'>
     <Hero/>
     <BentoGrid/>
   </main>
