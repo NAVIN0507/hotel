a6c17f34efa71ea5fd0935f7ba7ae6babc46f106
NAVIN0507 <navinofficial2005@gmail.com>
2025-04-30 12:46:26 +0530

    Added testmonials section

--- components/Home/Amunities.tsx
+++ components/Home/Amunities.tsx
@@ -5,13 +5,13 @@ import React from 'react'
 const Amunities = () => {
   return (
   <section className=' pl-14 pr-14'>
-    <div className=' flex items-center justify-between gap-1'>
+    <div className=' flex items-center justify-between gap-1 max-sm:text-center'>
         <div className='flex flex-col gap-1'>
             <p className='text-sm text-[#5C5C5C]'>AMUNITIES</p>
-            <h1 className='flex gap-1 font-mono text-4xl text-[#45443F]'><div className="w-20 mt-5  hidden lg:block  ml-5  h-0 border border-gray-500" /> MADE FOR</h1>
+            <h1 className='flex gap-1 font-mono text-4xl text-[#45443F] max-sm:ml-8'><div className="w-20 mt-5  hidden lg:block  ml-5  h-0 border border-gray-500" /> MADE FOR</h1>
             <h1 className='font-mono text-4xl text-[#45443F]'>YOUR COMFORT</h1>
         </div>
-        <div className='w-[300px]'>
+        <div className='w-[300px] hidden md:block'>
             <p className='text-sm text-[#3A3A3A]'>Spoil yourself with the assortment in cuisine and taste. Explore & Investigate the wide range of food you'd be honored with no place else.
 </p>
         </div>
