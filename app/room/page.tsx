348ba62e61ccf10eeea54188c703513f41059570
NAVIN0507 <navinofficial2005@gmail.com>
2025-04-30 20:13:31 +0530

    Added Hero section

--- app/room/page.tsx
+++ app/room/page.tsx
@@ -1,3 +1,4 @@
+import Hero from '@/components/Room/Hero'
 import NavBar from '@/components/Room/NavBar'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const page = () => {
   return (
    <main className='p-4 flex flex-col gap-10 mb-20'>
     <NavBar/>
+    <Hero/>
    </main>
   )
 }
---
+++ components/Room/Hero.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Hero = () => {
+  return (
+   <section className='max-w-screen'>
+    <div className='flex gap-2'>
+        <Image
+        src={"/assets/hero_bg.jpg"}
+        alt="hero"
+        width={1000}
+        height={1000}
+        className='rounded-2xl'
+        />
+        <div className='h-full relative'>
+          <Image
+        src={"/assets/hero_bg.jpg"}
+        alt="hero"
+        width={1000}
+        height={1000}
+        className='rounded-2xl h-[550px] z-1'
+        />
+       <button className="absolute bottom-4 left-4  bg-white/50 text-white/95 text-sm px-4 py-2 rounded-full opacity-80 hover:opacity-100">
+              ▶  PLAY VIDEO
+            </button>
+        </div>
+    </div>
+   </section>
+  )
+}
+
+export default Hero
