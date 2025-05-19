fb96b789041e7a07393bf3bb9ca9a9799829987e
NAVIN0507 <navinofficial2005@gmail.com>
2025-05-19 18:10:11 +0530

    Added form

--- .next/static/chunks/app_page_tsx_b2f3bef0._.js
+++ .next/static/chunks/app_page_tsx_b2f3bef0._.js
@@ -5,7 +5,7 @@
   "static/chunks/node_modules_framer-motion_dist_es_82de910d._.js",
   "static/chunks/node_modules_motion-dom_dist_es_eae4c9f6._.js",
   "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
-  "static/chunks/node_modules_b5dde6e9._.js",
+  "static/chunks/node_modules_f6c40f4f._.js",
   "static/chunks/_f122f2a3._.js"
 ],
     source: "dynamic"
