0b43ef36014e719ac41ed8be62cdbea90c29bcb1
NAVIN0507 <navinofficial2005@gmail.com>
2025-11-07 14:51:53 +0530

    updated out

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,18 @@
 "use client"
 import { Loader2Icon } from 'lucide-react'
-import Link from 'next/link'
-import { redirect } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
- 
+
 export default function NotFound() {
+  const router = useRouter()
+
   useEffect(()=>{
-    setTimeout(()=>{
-      return redirect("/")
-    } , 2000)
-  } , [])
+    const t = setTimeout(()=>{
+      router.push("/")
+    }, 2000)
+    return ()=> clearTimeout(t)
+  }, [router])
+
   return (
     <div className='flex w-full items-center justify-center min-h-screen'>
       <Loader2Icon className='size-10 animate-spin text-yellow-500'/>
